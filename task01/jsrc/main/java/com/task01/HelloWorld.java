0296b6ecd7585f25cc0725b473b5d0aa7cfd8ac0
Arman Abrahamyan <Arman.Abrahamyan@wolterskluwer.com>
2024-04-26 10:31:14 +0400

    fix structure

--- task01/jsrc/main/java/com/task01/HelloWorld.java
+++ task01/jsrc/main/java/com/task01/HelloWorld.java
@@ -1,10 +1,12 @@
 package com.task01;
 
-import com.amazonaws.services.lambda.runtime.Context;
+
+
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
+import javax.naming.Context;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,9 +18,10 @@ import java.util.Map;
 )
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
+	@Override
+	public Map<String, Object> handleRequest(Object o, com.amazonaws.services.lambda.runtime.Context context) {
 		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
+		Map<String, Object> resultMap = new HashMap<>();
 		resultMap.put("statusCode", 200);
 		resultMap.put("message", "Hello from Lambda");
 		return resultMap;
--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -1,9 +1,17 @@
 package com.task02;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,12 +29,21 @@ import java.util.Map;
 public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
 	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+		LambdaLogger logger = context.getLogger();
 		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
-		Map<String, Object> resultMap = new HashMap<String, Object>();
+		Map<String, Object> resultMap = new HashMap<>();
 		resultMap.put("statusCode", 200);
 		resultMap.put("message", "Hello from Lambda");
-		response.setStatusCode(200);
-		response.setBody(resultMap.toString());
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			String responseJson = objectMapper.writeValueAsString(resultMap);
+			logger.log(responseJson);
+			response.setStatusCode(200);
+			response.setBody(responseJson);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
 		return response;
 	}
 }
