1c4661700a8ec79ee996a0061abbe361e6eae981
Arman Abrahamyan <Arman.Abrahamyan@wolterskluwer.com>
2024-05-15 23:11:10 +0400

    task06

---
+++ task06/jsrc/main/java/com/task06/AuditProducer.java
@@ -0,0 +1,66 @@
+package com.task06;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent.DynamodbStreamRecord;
+import com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+@LambdaHandler(lambdaName = "audit_producer",
+	roleName = "audit_producer-role",
+	isPublishVersion = false,
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+public class AuditProducer implements RequestHandler<DynamodbEvent, Void> {
+
+	private static final Logger logger = Logger.getLogger(AuditProducer.class.getName());
+	private static final String INSERT_EVENT = "INSERT";
+	private static final String MODIFY_EVENT = "MODIFY";
+	private static final String TABLE_NAME = "cmtr-a7a5b08f-Audit-test";
+
+	private final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+	private final DynamoDB dynamoDB = new DynamoDB(client);
+	public Void handleRequest(DynamodbEvent dynamodbEvent, Context context) {
+		logger.info("Received event: " + dynamodbEvent);
+		List<DynamodbStreamRecord> records = dynamodbEvent.getRecords();
+		Table table = dynamoDB.getTable(TABLE_NAME);
+		for (DynamodbStreamRecord record : records) {
+			if (record.getEventName().equals(INSERT_EVENT)) {
+				Map<String, AttributeValue> newImage = record.getDynamodb().getNewImage();
+				Item item = new Item().withPrimaryKey("id", UUID.randomUUID().toString())
+						.withString("itemKey", String.valueOf(newImage.get("key")))
+						.withString("modificationTime", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME))
+						.withMap("newValue", Map.of("key", String.valueOf(newImage.get("key")), "value",
+								Integer.parseInt(String.valueOf(newImage.get("value")))));
+				table.putItem(item);
+			}
+			if (record.getEventName().equals(MODIFY_EVENT)) {
+				Map<String, AttributeValue> newImage = record.getDynamodb().getNewImage();
+				Map<String, AttributeValue> oldImage = record.getDynamodb().getOldImage();
+				Item item = new Item().withPrimaryKey("id", UUID.randomUUID().toString())
+						.withString("itemKey", String.valueOf(oldImage.get("key")))
+						.withString("modificationTime", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME))
+						.withString("updatedAttribute", "value")
+						.withInt("oldValue", Integer.parseInt(String.valueOf(oldImage.get("value"))))
+						.withInt("newValue", Integer.parseInt(String.valueOf(newImage.get("value"))));
+				table.putItem(item);
+			}
+		}
+		return null;
+	}
+}
